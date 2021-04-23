afdc8233a86c735ee8fff1448d87c9a8dd61bd8e
Elisha Hollander <just4now666666@gmail.com>
2021-04-23 14:02:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from lumigo_tracer import LumigoChalice
-from chalice import Chalice
 import json
-app = Chalice(app_name = "appname")
-app = LumigoChalice(app, token = YOUR_LUMIGO_KEY)
+app = __import__('lumigo_tracer').LumigoChalice(__import__('chalice').Chalice(app_name = "appname"), token = YOUR_LUMIGO_KEY)
 @app.lambda_function()
 def func(event, context):return{'statusCode':200,'body':json.dumps('Hi!')}
 @app.route("/")
